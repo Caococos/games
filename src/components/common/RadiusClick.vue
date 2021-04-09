990866b6632870a504643fcacd4cebbcb351e66e
zhihaot1 <493524674@qq.com>
2021-04-09 22:46:44 +0800

    实现简单版的打地鼠小游戏

---
+++ src/components/common/RadiusClick.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="btn-container">
+    <img class="svg" src="~@/assets/img/shideright39.svg" alt="">
+    <button>{{message}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RadiusClick",
+  props: {
+    message: {
+      type: String,
+      default: 'Hover me'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.btn-container {
+  position: relative;
+}
+
+button {
+  border: 0;
+  border-radius: 50px;
+  color: white;
+  background: #5f55af;
+  padding: 15px 20px 15px 60px;
+  /*text-transform: uppercase;*/
+  background: linear-gradient(to right, #f72585 50%, #5f55af 50%);
+  background-size: 200% 100%;
+  background-position: right bottom;
+  transition:all 2s ease;
+}
+
+.svg {
+  background: #f72585;
+  padding: 16px;
+  border-radius: 50%;
+  position: absolute;
+  left: 0;
+  top: 0%;
+}
+
+button:hover {
+  background-position: left bottom;
+}
+</style>
